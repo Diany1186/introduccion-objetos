2e9ff6238bb61006be92b1543e12d19633497402
DianyLocal <diany1186@hotmail.com>
2023-04-13 13:50:43 -0500

    Utilizando objetos estáticos

--- CuentaCorriente.js
+++ CuentaCorriente.js
@@ -5,6 +5,7 @@ export class CuentaCorriente {
     numero;
     agencia;
     #saldo;//atributo privado, solo puede ser modificado por la clase, también se puede poner _antes del nombre para que otros desarrolladores lo traten como privado, pero js no lo trata como privado
+    static cantidadCuentas = 0;//hace que el atributo cantidadCuentas sea común para todos los objetos de este tipo (es un atributo estático)
    
     set cliente(valor) {
         if (valor instanceof Cliente)//evita que al atributo cliente se le pueda asignar otro valor que no sea un objeto Cliente
@@ -20,6 +21,7 @@ export class CuentaCorriente {
         this.numero = numero;
         this.agencia =agencia;
         this.#saldo = 0;
+        CuentaCorriente.cantidadCuentas++;
     }
 
     depositoEnCuenta(valor) {
--- index.js
+++ index.js
@@ -3,20 +3,16 @@ import {Cliente} from "./Cliente.js";
 import {CuentaCorriente} from "./CuentaCorriente.js";
 
 const cliente = new Cliente("Leonardo","1231231231","1231231");
-
 const cuentaLeonardo = new CuentaCorriente(cliente,"1","001");
 
-let saldo = cuentaLeonardo.verSaldo();
-
-saldo = cuentaLeonardo.depositoEnCuenta(200);
-saldo = cuentaLeonardo.retirarDeCuenta(50);
-console.log(`El saldo actual de Leonardo es ${saldo}`);
-
 const cliente2 = new Cliente("Sofía","1231231232","1231232");
-
 const cuentaSofia = new CuentaCorriente(cliente2,"2","002");
 
+let saldo = cuentaLeonardo.verSaldo();
+saldo = cuentaLeonardo.depositoEnCuenta(200);
+saldo = cuentaLeonardo.retirarDeCuenta(50);
+console.log(`El saldo actual de Leonardo es ${cuentaLeonardo.verSaldo()}`);
 
-console.log(cuentaLeonardo.cliente);
-console.log(cuentaSofia.cliente);
+console.log(CuentaCorriente.cantidadCuentas);
+
 
